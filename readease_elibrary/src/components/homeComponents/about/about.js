007e9201401ebd03b254c1e5551e1afe67a2ba78
Younes <Younssyounss834@gmail.com>
2024-04-08 23:35:08 +0000

    add the about section

--- readease_elibrary/src/components/homeComponents/about/about.js
+++ readease_elibrary/src/components/homeComponents/about/about.js
@@ -1,7 +1,19 @@
+import "../../../index2.css";
 import "./about.css";
+import image  from "../../../images/about-1-835x418.jpg";
 
 function About() {
-  return <div></div>;
+  return <div>
+    <div className="container">
+           <h2 className="section__title"  id="title">  About Us</h2>
+          <div className='img' > <img id='image'   src= {image} alt=" Ok" /></div>
+          <div className='parag'><p>Readease was born out of the shared passion and vision of three students determined to make reading an effortless and exhilarating experience for all. Our mission is simple yet powerful: to transform the way people engage with books and ignite a love for reading that knows no bounds.
+
+At Readease, we believe that reading is more than just words on a page—it is an immersive journey of discovery and wonder. Our magical library beckons you to embark on that journey, where you can explore captivating tales, uncover hidden gems, and immerse yourself in the joy of turning pages. We invite you to join us as we celebrate the sheer magic and power of words. 📚✨</p></div>
+               
+                
+         </div>
+  </div>;
 }
 
 export default About;
